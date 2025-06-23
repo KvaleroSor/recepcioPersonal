4d1d8be8401a6441a79d80c6b5c448e75b7cb6c4
kvalerosor <kvalerosor@hotmail.com>
2025-06-23 17:35:30 +0200

    fix: explicit import for Paqueteria component

--- recepcionpersonal/src/App.js
+++ recepcionpersonal/src/App.js
@@ -1,7 +1,7 @@
 import "./styles/App.scss";
 import ButtonPaqueteria from "./components/ButtonPaqueteria";
 import ButtonComerciales from "./components/ButtonComerciales";
-import Paqueteria from "./components/Paqueteria";
+import Paqueteria from "./components/Paqueteria.js";
 import Comerciales from "./components/Comerciales";
 import { Route, Routes } from "react-router-dom";
 // import logoImasd from './logo/logoImasd.jpeg';
