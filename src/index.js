bfd1257784f1aa2b8d39ffde7304bf5ab9631bbe
Kvalerosor <kvalerosor@hotmail.com>
2025-06-26 14:16:37 +0200

    feat: initialize React app with Vercel Analytics and Speed Insights integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./styles/App.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +12,7 @@ root.render(
         <BrowserRouter>
             <App />
             <Analytics />
+            <SpeedInsights />
         </BrowserRouter>
     </React.StrictMode>
 );
