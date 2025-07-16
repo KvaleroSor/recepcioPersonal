87e63317e50cf5b76dd16d2b740f4fbe7000f1c6
kvalerosor <kvalerosor@hotmail.com>
2025-07-16 18:03:56 +0200

    refactor: simplify button click handling and remove event listeners in ButtonGroup component

--- client/src/features/buttonGroup-selectSearch/components/ButtonGroup.js
+++ client/src/features/buttonGroup-selectSearch/components/ButtonGroup.js
@@ -1,61 +1,28 @@
 import React, { useState } from "react";
 
 const ButtonGroup = () => {
-    // const [isButtonComercialClicked, setIsButtonComercialClicked] = useState(null);
-    const [isButtonEmpresaClicked, setIsButtonEmpresaClicked] = useState(null);
-    const [isContadorComercial, setIsContadorComercial] = useState(false);
-    // const [isContadorEmpresa, setIsContadorEmpresa] = useState(false);
+    const [isButtonComercialClicked, setIsButtonComercialClicked] = useState(true);
+    const [isButtonEmpresaClicked, setIsButtonEmpresaClicked] = useState(true);    
 
-    const handleClickComercial = () => {
-        setIsContadorComercial(!isContadorComercial);
-
-        if (isContadorComercial) {
-            document.addEventListener("click", (e) => {
-                const buttonComercial = e.target;
-
-                buttonComercial.classList.add("button_background-add");
-                buttonComercial.removeEventListener("click", handleClickComercial);
-            });
-        } else {
-            document.addEventListener("click", (e) => {
-                const buttonComercial = e.target;
-
-                buttonComercial.classList.remove("button_background-add");
-                buttonComercial.removeEventListener("click", handleClickComercial);
-            });
-        }
+    const handleClick = (buttonType) => {                     
+        /**
+         * Gestionar el estado y color del botón desde el handleClick 
+         * modificando directamente la clase.
+         * 
+         * NO UTILIZAR ADDEVENTLISTENER ❌
+         */
     };
-
-    const handleClickEmpresa = () => {
-        setIsButtonEmpresaClicked(!isButtonEmpresaClicked);
-
-        if (isButtonEmpresaClicked) {
-            document.addEventListener("click", (e) => {
-                const buttonEmpresa = e.target;
-
-                buttonEmpresa.classList.add("button_background-add");
-                buttonEmpresa.removeEventListener("click", handleClickEmpresa);
-            });
-        } else {
-            document.addEventListener("click", (e) => {
-                const buttonEmpresa = e.target;
-
-                buttonEmpresa.classList.remove("button_background-add");
-                buttonEmpresa.removeEventListener("click", handleClickEmpresa);
-            })
-        }
-    }
 
     return (
         <div className="container-buttonGroup">
             <button 
                 id="buttonComercial" 
                 className="button-styles_buttonGroup button-margin_left--setup"
-                onClick={handleClickComercial}>Comerciales</button>
+                onClick={handleClick}>Comerciales</button>
             <button 
                 id="buttonEmpresa" 
                 className="button-styles_buttonGroup" 
-                onClick={handleClickEmpresa}>Empresa</button>
+                onClick={handleClick}>Empresa</button>
         </div>
     );
 };
