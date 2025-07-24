bc4641174cc5acb808c97c09430340371be89f0f
kvalerosor <kvalerosor@hotmail.com>
2025-07-24 21:14:22 +0200

    security: replace hardcoded Firebase credentials with environment variables

--- client/src/db/firebaseConfig.js
+++ client/src/db/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOWXRQwz7eMrt0DT1L8L8ewsgqDOhYT3w",
-  authDomain: "recepcionpersonal-a3296.firebaseapp.com",
-  projectId: "recepcionpersonal-a3296",
-  storageBucket: "recepcionpersonal-a3296.firebasestorage.app",
-  messagingSenderId: "279043712411",
-  appId: "1:279043712411:web:ac668cd8ce0f3a24026d3e",
-  measurementId: "G-SG64N735XX"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
